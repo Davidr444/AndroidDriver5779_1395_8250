30de7b63d34c64e0814a6244fc8b493f56816d24
David <davidr0141@gmail.com>
2019-01-08 19:04:06 +0200

    Start Design the Register activity. Add OpeningActivity. didnt check yet.

---
+++ app/src/main/java/com/jct/davidandyair/androiddriver5779_1395_8250/controller/LoginActivity.java
@@ -0,0 +1,55 @@
+package com.jct.davidandyair.androiddriver5779_1395_8250.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.jct.davidandyair.androiddriver5779_1395_8250.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText usName;
+    private EditText pssd;
+    private Button enter;
+    private String userName;
+    private String password;
+
+    private boolean checkIdentity(String usN, String p)
+    {
+        //needs to check on the firebase whether the details are correct!
+
+        return true;
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        findViews();
+    }
+
+    private void findViews()
+    {
+        usName = findViewById(R.id.editText);
+        pssd = findViewById(R.id.editText2);
+        enter = findViewById(R.id.button);
+
+        enter.setOnClickListener(new View.OnClickListener() {
+                                     @Override
+                                     public void onClick(View v) {
+                                         userName = usName.getText().toString();
+                                         password = pssd.getText().toString();
+
+                                         checkIdentity(userName, password);
+                                         //change activity ETC...
+                                     }
+                                 }
+
+        );
+
+    }
+}
---
+++ app/src/main/java/com/jct/davidandyair/androiddriver5779_1395_8250/controller/OpeningActivity.java
@@ -0,0 +1,45 @@
+package com.jct.davidandyair.androiddriver5779_1395_8250.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.jct.davidandyair.androiddriver5779_1395_8250.R;
+
+public class OpeningActivity extends AppCompatActivity {
+    AnimationDrawable openingAnimation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opening);
+
+        ImageView imageView = (ImageView)findViewById(R.id.animImage);
+        imageView.setBackgroundResource(R.drawable.opening_animation);
+        openingAnimation = (AnimationDrawable) imageView.getBackground();
+        openingAnimation.start();
+
+        Thread myThread = new Thread()
+        {
+            @Override
+            public void run()
+            {
+                try {
+                    sleep(4500);
+                    Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+
+
+}
