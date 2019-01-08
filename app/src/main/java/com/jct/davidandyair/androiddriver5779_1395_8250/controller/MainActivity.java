1cbd94f266df0beb25e9fdf07689711e06d06576
yair2 <ybendavi@g.jct.ac.il>
2019-01-08 08:59:13 +0200

    i started working on the logical part of the main activity

--- app/src/main/java/com/jct/davidandyair/androiddriver5779_1395_8250/controller/MainActivity.java
+++ app/src/main/java/com/jct/davidandyair/androiddriver5779_1395_8250/controller/MainActivity.java
@@ -2,10 +2,27 @@ package com.jct.davidandyair.androiddriver5779_1395_8250.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.jct.davidandyair.androiddriver5779_1395_8250.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText usName;
+    private EditText pssd;
+    private Button enter;
+    private String userName;
+    private String password;
+
+    private boolean checkIdentity(String usN, String p)
+    {
+        //needs to check on the firebase whether the details are correct!
+
+        return true;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +34,22 @@ public class MainActivity extends AppCompatActivity {
 
     private void findViews()
     {
+        usName = findViewById(R.id.editText);
+        pssd = findViewById(R.id.editText2);
+        enter = findViewById(R.id.button);
+
+        enter.setOnClickListener(new View.OnClickListener() {
+                                     @Override
+                                     public void onClick(View v) {
+                                         userName = usName.getText().toString();
+                                         password = pssd.getText().toString();
+
+                                         checkIdentity(userName, password);
+                                         //change activity ETC...
+                                     }
+                                 }
+
+        );
 
     }
 }
