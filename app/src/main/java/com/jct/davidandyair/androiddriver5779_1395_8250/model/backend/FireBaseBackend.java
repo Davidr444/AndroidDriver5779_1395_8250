863ae09e33bf623b4044aabe42219e3fe3c182de
yair2 <ybendavi@g.jct.ac.il>
2019-01-09 20:00:54 +0200

    what we did together

--- app/src/main/java/com/jct/davidandyair/androiddriver5779_1395_8250/model/backend/FireBaseBackend.java
+++ app/src/main/java/com/jct/davidandyair/androiddriver5779_1395_8250/model/backend/FireBaseBackend.java
@@ -1,24 +1,31 @@
 package com.jct.davidandyair.androiddriver5779_1395_8250.model.backend;
 
+import android.location.Address;
 import android.support.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.jct.davidandyair.androiddriver5779_1395_8250.model.entities.Drive;
 import com.jct.davidandyair.androiddriver5779_1395_8250.model.entities.Driver;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class FireBaseBackend implements IBackend {
-    FirebaseDatabase database;
+    static FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private static ChildEventListener drivesRefChildEventList;
+    //The list of the drives the will be return
+    static ArrayList<Drive> driveList = new ArrayList<Drive>();
 
   public interface Action<T>
   {
@@ -32,10 +39,72 @@ public class FireBaseBackend implements IBackend {
       void onFailure(Exception exception);
   }
 
+  public static void notifyToDrivesList(final NotifyDataChange<List<Drive>> notifyDataChange){
+      DatabaseReference DrivesRef = database.getReference("drives");
+      if (notifyDataChange != null)
+      {
+          if (drivesRefChildEventList != null)
+          {
+              notifyDataChange.onFailure(new Exception("first unNotify student list"));
+              return;
+          }
+          driveList.clear();
+          drivesRefChildEventList = new ChildEventListener() {
+              @Override
+              public void onChildAdded(DataSnapshot dataSnapshot, String s)
+              {
+                  Drive drive = dataSnapshot.getValue(Drive.class);
+                  String id = dataSnapshot.getKey();
+                  drive.setId(Long.parseLong(id));
+                  driveList.add(drive);
+                  notifyDataChange.OnDataChanged(driveList);
+              }
+              @Override
+              public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                  Drive drive = dataSnapshot.getValue(Drive.class);
+                  Long id = Long.parseLong(dataSnapshot.getKey());
+                  drive.setId(id);
+                  for (int i = 0; i < driveList.size(); i++)
+                  {
+                      if (driveList.get(i).getId().equals(id))
+                      {
+                          driveList.set(i, student);
+                          break;
+                      }
+                  }
+                  notifyDataChange.OnDataChanged(driveList);
+              }
+              @Override
+              public void onChildRemoved(DataSnapshot dataSnapshot)
+              {
+                  Drive drive = dataSnapshot.getValue(Drive.class);
+                  Long id = Long.parseLong(dataSnapshot.getKey());
+                  drive.setId(id);
+                  for (int i = 0; i < driveList.size(); i++)
+                  {
+                      if (driveList.get(i).getId() ==  id)
+                      {
+                          driveList.remove(i);
+                          break;
+                      }
+                  }
+                  notifyDataChange.OnDataChanged(driveList);
+              }
+              @Override
+              public void onChildMoved(DataSnapshot dataSnapshot, String s)
+              { }
+              @Override
+              public void onCancelled(DatabaseError databaseError) {
+                  notifyDataChange.onFailure(databaseError.toException());
+              }
+          };
+          DrivesRef.addChildEventListener(drivesRefChildEventList);
+      }
+  }
+  public static void stopNotifyToDrivesList(){}
+
   @Override
     public void addDriver(final Driver drive, final Action<Long> action) {
-        // Write a message to the database
-        database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("drivers");
 
         myRef.push().setValue(drive).addOnSuccessListener(new OnSuccessListener<Void>() {
@@ -54,9 +123,43 @@ public class FireBaseBackend implements IBackend {
     }
 
     @Override
+    public ArrayList<Drive> getUnhandledDrives(Action<Long> action) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Drive> getFinishedDrives(Action<Long> action) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Drive> getDriversDrives(Driver d, Action<Long> action) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Drive> getDrivesByCity(Address address, Action<Long> action) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Drive> getDrivesByDistance(Driver d, float distance, Action<Long> action) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Drive> getDrivesByDate(Date date, Action<Long> action) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Drive> getDrivesByPrice(float price, Action<Long> action) {
+        return null;
+    }
+
+    @Override
     public ArrayList<Driver> getDrivers(final Action<Long> action) {
-        // Get the Firebase reference
-        database = FirebaseDatabase.getInstance();
+
         DatabaseReference myRef = database.getReference("drivers");
 
         //The list of the drivers the will be return
--- app/src/main/java/com/jct/davidandyair/androiddriver5779_1395_8250/model/backend/IBackend.java
+++ app/src/main/java/com/jct/davidandyair/androiddriver5779_1395_8250/model/backend/IBackend.java
@@ -1,10 +1,22 @@
 package com.jct.davidandyair.androiddriver5779_1395_8250.model.backend;
 
+import android.location.Address;
+
+import com.jct.davidandyair.androiddriver5779_1395_8250.model.entities.Drive;
 import com.jct.davidandyair.androiddriver5779_1395_8250.model.entities.Driver;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public interface IBackend {
-    public void addDriver(final Driver d, final FireBaseBackend.Action<Long> action);
-    public ArrayList<Driver> getDrivers(  final FireBaseBackend.Action<Long> action);
+
+    ArrayList<Driver> getDrivers(final FireBaseBackend.Action<Long> action);
+    void addDriver(final Driver d, final FireBaseBackend.Action<Long> action);
+    ArrayList<Drive> getUnhandledDrives(final FireBaseBackend.Action<Long> action);
+    ArrayList<Drive> getFinishedDrives(final FireBaseBackend.Action<Long> action);
+    ArrayList<Drive> getDriversDrives(final Driver d, final FireBaseBackend.Action<Long> action);
+    ArrayList<Drive> getDrivesByCity(Address address, final FireBaseBackend.Action<Long> action);
+    ArrayList<Drive> getDrivesByDistance(final Driver d, final float distance, final FireBaseBackend.Action<Long> action);
+    ArrayList<Drive> getDrivesByDate(final Date date, final FireBaseBackend.Action<Long> action);
+    ArrayList<Drive> getDrivesByPrice(final float price, final FireBaseBackend.Action<Long> action);
 }
