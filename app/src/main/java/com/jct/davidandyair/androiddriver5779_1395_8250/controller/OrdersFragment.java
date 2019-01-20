0ce3a0fb3bdeaebe9f91b977d5c0614b632c6337
David <davidr0141@gmail.com>
2019-01-20 21:19:06 +0200

    Merge Versions

---
+++ app/src/main/java/com/jct/davidandyair/androiddriver5779_1395_8250/controller/OrdersFragment.java
@@ -0,0 +1,20 @@
+package com.jct.davidandyair.androiddriver5779_1395_8250.controller;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jct.davidandyair.androiddriver5779_1395_8250.R;
+
+public class OrdersFragment extends Fragment {
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_orders,container,false);
+        return view;
+    }
+}
